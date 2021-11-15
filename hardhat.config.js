d40d63f4e24b0bd4d5dc66eea7eef27f11ca989a
Erich <ecruz@bachecubano.com>
2021-11-15 16:52:50 -0500

    cleanup private keys

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,10 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
+const projectId = process.env.INFURA_PROJECT_ID;
+const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY;
+
+/*
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -12,10 +17,19 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
+*/
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${projectId}`,
+      accounts: [
+        privateKey
+      ]
+    }
+  }
 };
